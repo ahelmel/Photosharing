9551bc2770b5b5b8e77f8cb1e54a0ab15e250788
Arthur Helmel <ahelmel@gmx.de>
2011-05-30 23:05:31 +0200

    add custom link_renderer for will_paginate plugin

--- config/environment.rb
+++ config/environment.rb
@@ -7,5 +7,4 @@ Photosharing::Application.initialize!
 # Initialize pagination
 WillPaginate::ViewHelpers.pagination_options[:prev_label] = "< Zurück"
 WillPaginate::ViewHelpers.pagination_options[:next_label] = "Vor >"
-WillPaginate::ViewHelpers.pagination_options[:inner_window] = 1
-WillPaginate::ViewHelpers.pagination_options[:outer_window] = 1
+WillPaginate::ViewHelpers.pagination_options[:renderer] = 'MinimalPaginationLinkRenderer'
